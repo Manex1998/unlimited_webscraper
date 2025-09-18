b9adfa36f8ce1b30e4912feb781c58d5230289dc
Anesu <emmamukwa@gmail.com>
2025-09-18 09:55:45 +0200

    added a comment

--- SALEADS.py
+++ SALEADS.py
@@ -1,3 +1,4 @@
+#working code for scraping sabusinessdirectories.com with improved logic to find business names and contact details
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
